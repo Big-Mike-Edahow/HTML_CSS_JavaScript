bd1fd6ee98de014b227ad22ce2934276146ff762
Mike Jackson <perigran_falcon@yahoo.com>
2023-06-25 07:44:57 -0600

    Add files via upload

--- Chapter_06/echo.py
+++ Chapter_06/echo.py
@@ -9,17 +9,22 @@ import cgi
 form = cgi.FieldStorage( )
 
 print('<!DOCTYPE HTML>')
+print('<html lang="en">')
 print('<html>')
 print('<title>Web Server Response</title>')
-print('<style>tr,th,td{border:2px solid Gray})</style>')
-print( '<table style="width:500px"><tr><th>Key<th>Value' )
+print('<style>tr, th, td {border: 2px solid Gray;}')
+print('body {background-color: BlanchedAlmond;}</style>')
+print('<table style="width:500px">')
+print('<tr><th>Key</th><th>Value</th></tr>')
+print('<tr>')
 
 for i in form.keys( ):
   key = str( i )
   val = str( form.getvalue( key ) )
-  print( '<tr><td>' + key + '<td>' + val )
+  print( '<td>' + key + '</td>' + '<td>' + val + '</td>' )
   
 print('</tr>')
 print('</table>')    
-print ('<img src=\"https://big-mike-edahow.github.io/images/Apache-Server.jpg\" width=\"160px\" height=\"120px\">')
+print ('''<img src=\"http://big-mike-edahow.github.io/images/Apache-Server.jpg\"
+width=\"160px\" height=\"120px\">''')
 print('</html>')
