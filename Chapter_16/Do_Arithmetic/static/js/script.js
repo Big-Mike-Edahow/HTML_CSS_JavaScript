15a9ab9a8919b83a614c19486138c8be2dc6abed
Mike Jackson <perigran_falcon@yahoo.com>
2023-12-28 08:14:13 -0700

    Add files via upload

---
+++ Chapter_16/Do_Arithmetic/static/js/script.js
@@ -0,0 +1,29 @@
+// script.js
+
+(function () {
+    let sum = 80;
+
+    console.log("Value of sum: " + sum);
+
+    let add = sum + 20;
+    console.log("Addition: sum + 20: " + add);
+
+    let sub = sum - 50;
+    console.log("Subtraction: sum - 50: " + sub);
+
+    let mul = sum * 5;
+    console.log("Multiplication: sum * 5: " + mul);
+
+    let div = sum / 4;
+    console.log("Division: sum / 4; " + div);
+
+    let mod = sum % 2;
+    console.log("Modulus: sum % 2: " + mod);
+
+    let inc = ++sum;
+    console.log("Increment: sum++: " + inc);
+
+    let dec = --sum;
+    console.log("Decrement: sum--: " + dec);
+})();
+
