34417cbd7d97fa651a51c5d5a084f6cc2c7fcbff
Mike Jackson <perigran_falcon@yahoo.com>
2024-01-08 15:05:25 -0700

    Add files via upload

---
+++ Chapter_21/Reset_Changes/static/js/script.js
@@ -0,0 +1,30 @@
+// script.js
+
+function reactTo(event, info) {
+    info.innerHTML = event.type;
+}
+
+function defaultMessage(info) {
+    info.innerHTML = "Please enter your favorite coding language";
+}
+
+const form = document.getElementById("code");
+const lang = document.getElementById("lang");
+const info = document.getElementById("info");
+
+lang.addEventListener("focus", function (event) {
+    reactTo(event, info);
+});
+
+lang.addEventListener("focusout", function (event) {
+    reactTo(event, info);
+});
+
+form.addEventListener("reset", function () {
+    defaultMessage(info);
+});
+
+addEventListener("load", function () {
+    defaultMessage(info);
+});
+
